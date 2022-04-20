18a322c1c0a26c97df5056aa88fa4b7a5b78692c
Farhan Ishrak Islam <Niloy28@live.com>
2022-04-20 21:18:08 +0600

    Game logic done

---
+++ src/components/Square.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface IProps {
+	value: string;
+	onClick: () => void;
+};
+
+function Square(props: IProps) {
+		return <button className="square" onClick={props.onClick}>{props.value}</button>;
+}
+
+export default Square;
--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
-
-type SquareProps = {
-	value: number;
-};
-class Square extends React.Component<SquareProps> {
-	render() {
-		return <button className="square">{this.props.value}</button>;
-	}
-}
-
-class Board extends React.Component {
-	renderSquare(i: number) {
-		return <Square value={i} />;
-	}
-
-	render() {
-		const status = "Next player: X";
-
-		return (
-			<div>
-				<div className="status">{status}</div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
-			</div>
-		);
-	}
-}
+import { Board } from "./components/Board";
 
 class Game extends React.Component {
 	render() {
@@ -60,4 +21,6 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<Game />);
